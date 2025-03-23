50751258323390dad3346fb197e5a569d6bb4b85
Boopathy-senseai <boopathys@sense7ai.com>
2025-03-23 11:14:44 +0530

    deploy with new changes

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "github action run successfully, ci cd implementated"}
+    return {"message": "The GitHub Action ran successfully, and CI/CD has been implemented."}
